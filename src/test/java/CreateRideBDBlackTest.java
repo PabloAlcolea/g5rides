8927acc488afec51234de7c22b5ce8117be04eb6
ikerg <ikerg@LAPTOP-9A2MV3ED>
2024-09-30 14:48:35 +0200

    aaa

--- src/test/java/CreateRideBDBlackTest.java
+++ src/test/java/CreateRideBDBlackTest.java
@@ -23,7 +23,7 @@ public class CreateRideBDBlackTest {
 	 //sut:system under test
 	 static DataAccess sut=new DataAccess();
 	 //ddsa
-	 //AAAmAAAAAAAAAAAAAAs
+	 //AAAmAAAAAAAAAAAAAAsaa
 	 //additional operations needed to execute the test 
 	 static TestDataAccess testDA=new TestDataAccess();
 	 //DS
